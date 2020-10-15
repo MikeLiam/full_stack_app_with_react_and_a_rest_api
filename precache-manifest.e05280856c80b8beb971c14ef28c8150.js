c07f0d8f4ea7f048c36fc05bb9c2ac01479f106a
MikeLiam <miguelalcalaromero@gmail.com>
2020-10-15 18:53:01 +0200

    Updates

---
+++ precache-manifest.e05280856c80b8beb971c14ef28c8150.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1035d1d9cb11ca0ac87466b0421481f8",
+    "url": "/full_stack_app_with_react_and_a_rest_api/index.html"
+  },
+  {
+    "revision": "c787082afcc493e22f5c",
+    "url": "/full_stack_app_with_react_and_a_rest_api/static/css/main.b05ce75d.chunk.css"
+  },
+  {
+    "revision": "12ae7bede612b17ece3a",
+    "url": "/full_stack_app_with_react_and_a_rest_api/static/js/2.275dbe72.chunk.js"
+  },
+  {
+    "revision": "de81bfc3d566733bec20fcc6cadc32a6",
+    "url": "/full_stack_app_with_react_and_a_rest_api/static/js/2.275dbe72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c787082afcc493e22f5c",
+    "url": "/full_stack_app_with_react_and_a_rest_api/static/js/main.4935c02d.chunk.js"
+  },
+  {
+    "revision": "4123e850af681c0af390",
+    "url": "/full_stack_app_with_react_and_a_rest_api/static/js/runtime-main.4a411738.js"
+  }
+]);
