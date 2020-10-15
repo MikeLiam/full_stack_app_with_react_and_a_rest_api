791841a221c0220b354a5c2c4ae3e0ff587d094f
MikeLiam <miguelalcalaromero@gmail.com>
2020-10-15 19:00:24 +0200

    Updates

--- precache-manifest.ae1b7b73e881085dea368c56f4a8524c.js
+++ precache-manifest.ae1b7b73e881085dea368c56f4a8524c.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1887ed9f5e1982c554da4a3e8f8edb62",
+    "revision": "2ccba4e05d3ccca95c3a1c0e3929267d",
     "url": "/full_stack_app_with_react_and_a_rest_api/index.html"
   },
   {
-    "revision": "0d3e22140504ddb5a3d2",
+    "revision": "dda25b43e207e7228ade",
     "url": "/full_stack_app_with_react_and_a_rest_api/static/css/main.b05ce75d.chunk.css"
   },
   {
-    "revision": "12ae7bede612b17ece3a",
-    "url": "/full_stack_app_with_react_and_a_rest_api/static/js/2.275dbe72.chunk.js"
+    "revision": "4588fe62847af75bc692",
+    "url": "/full_stack_app_with_react_and_a_rest_api/static/js/2.e2eea7c4.chunk.js"
   },
   {
     "revision": "de81bfc3d566733bec20fcc6cadc32a6",
-    "url": "/full_stack_app_with_react_and_a_rest_api/static/js/2.275dbe72.chunk.js.LICENSE.txt"
+    "url": "/full_stack_app_with_react_and_a_rest_api/static/js/2.e2eea7c4.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "0d3e22140504ddb5a3d2",
-    "url": "/full_stack_app_with_react_and_a_rest_api/static/js/main.b9760efe.chunk.js"
+    "revision": "dda25b43e207e7228ade",
+    "url": "/full_stack_app_with_react_and_a_rest_api/static/js/main.ba4f7b18.chunk.js"
   },
   {
     "revision": "4123e850af681c0af390",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/full_stack_app_with_react_and_a_rest_api/precache-manifest.e64e282ab46e9a3ff4dd397f2b98a763.js"
+  "/full_stack_app_with_react_and_a_rest_api/precache-manifest.ae1b7b73e881085dea368c56f4a8524c.js"
 );
 
 self.addEventListener('message', (event) => {
